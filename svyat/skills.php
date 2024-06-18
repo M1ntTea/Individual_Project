6c60f955940e6fef7ff5825573673f3e352e34f4
M1ntTea <147503917+M1ntTea@users.noreply.github.com>
2024-06-18 04:09:10 -0700

    Add files via upload

---
+++ svyat/skills.php
@@ -0,0 +1,33 @@
+<?php include_once "./template/header.php"?>
+        <main class="portfolio" id='portfolio'>
+
+          <section class='skills' id='skills'>
+            <h2>Skills</h2>
+            <article class="skills-container">
+              <div class="skills__item" data-aos='fade-up'>
+                <img src="https://cdn-icons-png.flaticon.com/512/1199/1199124.png" alt="JS">
+                <p>Javascript</p>
+              </div>
+      
+              <div class="skills__item" data-aos='fade-up'>
+                <img src="https://cdn-icons-png.flaticon.com/512/1051/1051277.png" alt="HTML5">
+                <p>HTML5</p>
+              </div>
+      
+              <div class="skills__item" data-aos='fade-up'>
+                <img src="https://cdn-icons-png.flaticon.com/512/732/732190.png" alt="css">
+                <p>CSS3</p>
+              </div>
+      
+              <div class="skills__item" data-aos='fade-up'>
+                <img src="https://cdn-icons-png.flaticon.com/512/5968/5968705.png" alt="figma">
+                <p>Figma</p>
+              </div>
+      
+              <div class="skills__item" data-aos='fade-up'>
+                <img src="https://cdn-icons-png.flaticon.com/512/5968/5968559.png" alt="Github">
+                <p>Github</p>
+              </div>
+            </article>
+          </section>
+<?php include_once "./template/footer.php"?>
---
+++ svyat/template/header.php
@@ -0,0 +1,23 @@
+<?php define('ROOT', dirname(__DIR__));?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <!-- <link rel="stylesheet" href="<?= ROOT?>/style/style.css"> -->
+    <title>Svyatoslav Kolisnyk</title>
+</head>
+<body>
+    <div class='wrapper'>
+        <div class="container">
+          <header id="home">
+            <nav class="navbar">
+                <h2 class='name'><a href="./index.php">Svyatoslav Kolisnyk</a></h2>
+              <ul class="navbar__ul">
+                <li class='link'><a href="./index.php">Home</a></li>
+                <li class='link'><a href="./projects.php">Projects</a></li>
+                <li class='link'><a href="./skills.php">Skills</a></li>
+                <li class='link'><a href="#contact">Contact</a></li>
+              </ul>
+            </nav>
